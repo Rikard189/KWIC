ec8739646baf92d0d3ab386984474acf6b94c66c
Kevin Radtke <kevinradtkeg@gmail.com>
2020-05-21 01:08:59 -0500

    makefile y comments pa ber que peo

---
+++ kwic.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+
+  // llamada a separador lineas en lector de archivo -> vector de lineas
+
+  // for de vector de oraciones
+
+      // llamada a en formateador -> oracion sin mayuscula ni punto
+
+      // llamada a generacion de circular shift -> vector de oraciones shifteadas
+
+      // agrega `oraciones a vector principal
+
+  // sortear vector principal alfabeticamente -> KWIC
+
+  // llamada a output
+
+  // pasa la pipa
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+all:
+	@g++ -std=c++11 kwic.cpp -o kwic && ./ok
+run:
+	@./ok
