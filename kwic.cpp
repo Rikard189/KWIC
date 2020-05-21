4abd5d2ff239253d24c926aed9697d133c88e876
Kevin Radtke <kevinradtkeg@gmail.com>
2020-05-21 01:28:42 -0500

    proto

--- kwic.cpp
+++ kwic.cpp
@@ -5,15 +5,21 @@ using namespace std;
 
 int main() {
 
+  vector<string> result;
+
   // llamada a separador lineas en lector de archivo -> vector de lineas
+  vector<string> raw_sentences = {"Clouds are white.", "Pittsburgh is beautiful."};
 
-  // for de vector de oraciones
+  for (auto sentence : raw_sentences) {
 
-      // llamada a en formateador -> oracion sin mayuscula ni punto
+    // llamada a en formateador -> oracion sin mayuscula ni punto
 
-      // llamada a generacion de circular shift -> vector de oraciones shifteadas
+    // llamada a generacion de circular shift -> vector de oraciones shifteadas
+    vector<string> shift_sentences = {"clouds are white", "are white clouds", "white clouds are"};
 
-      // agrega `oraciones a vector principal
+    // agrega oraciones a vector principal
+    for (auto s : shift_sentences) result.push_back(s);
+  }
 
   // sortear vector principal alfabeticamente -> KWIC
 
