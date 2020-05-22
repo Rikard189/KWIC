bcf5c63b6dbff5fbf088e32304d6afa006db1f5b
Kevin Radtke <kevinradtkeg@gmail.com>
2020-05-21 20:14:49 -0500

    feat: stop words forking

--- CircularShift.h
+++ CircularShift.h
@@ -8,6 +8,7 @@ using namespace std;
 class CircularShift {
 public:
   vector<string> circ_shift(string s);
+  vector<string> stop_circ_shift(string s, set<string> stop_words);
 };
 
 vector<string> CircularShift::circ_shift(string s){
@@ -26,6 +27,39 @@ vector<string> CircularShift::circ_shift(string s){
        }
    }
    words.push_back(word);
+
+   string cur = "";
+   for (int i=0; i<words.size(); i++) {
+     for (int j=0; j<words.size(); j++) {
+       cur += words[(i+j)%words.size()] + " ";
+     }
+     cur.erase(cur.length()-1);
+     shift_result.push_back(cur);
+     cur = "";
+   }
+  return shift_result;
+}
+
+vector<string> CircularShift::stop_circ_shift(string s, set<string> stop_words){
+
+  vector<string> shift_result;
+  vector<string> words;
+
+  string word = "";
+   for (auto x : s) {
+       if (x == ' ') {
+         if (!stop_words.count(word)) {
+           words.push_back(word);
+         }
+         word = "";
+       }
+       else {
+         word = word + x;
+       }
+   }
+   if (!stop_words.count(word)) {
+     words.push_back(word);
+   }
 
    string cur = "";
    for (int i=0; i<words.size(); i++) {
