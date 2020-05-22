ea96c6b74d8c115f14c6e579db258cb83ad003be
Kevin Radtke <kevinradtkeg@gmail.com>
2020-05-21 19:33:22 -0500

    outputs to terminal also

--- kwic.cpp
+++ kwic.cpp
@@ -13,12 +13,13 @@ int main() {
   Output outputter;
 
   // lee input file y separa lineas
-  vector<string> raw_sentences = inputter.get_lines("input.txt");
+  vector<string> raw_sentences = inputter.get_lines("KWIC2_input1.txt");
 
   // corre el algoritmo kwic
   vector<string> sorted_sentences = kwic.kwic(raw_sentences);
 
   // output resultados
+  outputter.print_vector(sorted_sentences);
   outputter.output_txt(sorted_sentences);
 
 }
