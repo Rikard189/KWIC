a0667a6cb256eb860974c26747a01cb965737576
Ana Villarreal <ani.villarrealmtz@gmail.com>
2020-05-21 19:09:21 -0500

    ups

--- AlphabeticalOrder.h
+++ AlphabeticalOrder.h
@@ -10,7 +10,7 @@ public:
   vector<string> sort_sentences(vector<string> sentences);
 };
 
-vector<string> AlphabeticalOrder::sort_sentences(vector<string> sentences){
+vector<string> AlphabeticalOrder::sort_sentences(vector<string> sentences, bool asc){
   sort(sentences.begin(), sentences.end(), [](string s1, string s2) {
     return (asc ? s1 < s2 : s1 > s2);
   });
